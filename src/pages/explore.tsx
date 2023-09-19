863dd3219b9c6a13ac0b7a6c84205c1d2d2886b0
Diego-Batista <diegobtistadev@gmail.com>
2023-09-19 14:55:17 -0300

    Api routes to explore

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,14 +1,37 @@
+import { BookCard, BookWithAvgRating } from "@/components/BookCard";
 import { DefaultLayout } from "@/components/Layout/DefaultLayout";
 import { Input } from "@/components/Ui/Form/Input";
 import { PageTitle } from "@/components/Ui/PageTitle";
 import { Tag } from "@/components/Ui/Tag";
-import { ExploreContainer, TagsContainer } from "@/styles/pages/explore";
+import { api } from "@/lib/axios";
+import { BooksGrid, ExploreContainer, TagsContainer } from "@/styles/pages/explore";
 import { Binoculars, MagnifyingGlass } from "@phosphor-icons/react";
+import { Category } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { NextPageWithLayout } from "./_app";
 
 const ExplorePage: NextPageWithLayout = () => {
     const [search, setSearch] = useState('')
+    const [ selectedCategory, setSelectedCategory ] = useState< string | null >(null)
+
+    const { data: categories } = useQuery<Category[]>(["categories"], async () => {
+        const { data } = await api.get("/books/categories");
+        return data?.categories ?? []
+      })
+
+      const { data: books } = useQuery<BookWithAvgRating[]>(["books", selectedCategory], async () => {
+        const { data } = await api.get("/books", {
+          params: {
+            category: selectedCategory
+          }
+        });
+        return data?.books ?? []
+      })
+
+      const filteredBooks = books?.filter((book) => {
+        return book.name.toLowerCase().includes(search.toLowerCase()) || book.author.toLowerCase().includes(search.toLowerCase())
+      })
 
     return (
         <ExploreContainer>
@@ -27,13 +50,21 @@ const ExplorePage: NextPageWithLayout = () => {
             </header>
 
             <TagsContainer>
-                <Tag active>
-                    Todos
-                </Tag>
-                <Tag>
-                    Computação
-                </Tag>
+                    <Tag active={selectedCategory === null} onClick={() => setSelectedCategory(null)}>
+                        Tudo
+                    </Tag>
+                {categories?.map(category => (
+                    <Tag key={category.id} active={selectedCategory === category.id} onClick={() => setSelectedCategory(category.id)}>
+                        {category.name}
+                    </Tag>
+                ))}
             </TagsContainer>
+
+            <BooksGrid>
+                {filteredBooks?.map((book) => (
+                    <BookCard key={book.id} size="lg" book={book} />
+                ))}
+            </BooksGrid>
         </ExploreContainer>
     )
 }
